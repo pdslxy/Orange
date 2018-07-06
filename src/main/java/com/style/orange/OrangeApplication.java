702540544a3d7930321975a393eb96768079a409
lxy@enerbos.com <lxy@enerbos.com>
2018-07-06 17:31:07 +0800

    初始化

---
+++ src/main/java/com/style/orange/OrangeApplication.java
@@ -0,0 +1,16 @@
+package com.style.orange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Mr.Li
+ */
+@SpringBootApplication
+public class OrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeApplication.class, args);
+	}
+}
